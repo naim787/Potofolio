c09590e71332b1a4c214cf8b0b8db30cb0132ec8
Naim Abdullah <123891191+naim787@users.noreply.github.com>
2024-05-15 17:47:50 +0800

    membuat function untuk menambahkan data, ke channel

--- text.go
+++ text.go
@@ -3,8 +3,12 @@ import (
   "fmt"
   )
 
+func AddData(name string, channel chan string) {
+  channel <-name
+}
+
 func main() {
   channel := make(chan string)
-  channel <- "naim mulai menjadi masuk arena";
-  fmt.Println(<-channnel)
+  AddData("naim", cahnnel)
+  fmt.Println(channel)
   }
